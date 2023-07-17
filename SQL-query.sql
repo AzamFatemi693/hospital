7f442e0eae8786f2d02284ae1b4d875f660f259f
AzamFatemi693 <139552468+AzamFatemi693@users.noreply.github.com>
2023-07-17 23:58:29 +0200

    Update SQL-query.sql

--- SQL-query.sql
+++ SQL-query.sql
@@ -197,7 +197,10 @@ having count(*) > 1
 
 # Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated. Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.
 
-select concat(first_name, ' ', last_name) as full_name, 			round(height/30.48,1) as height , round(weight*2.205,0) as weight, birth_date,
+select concat(first_name, ' ', last_name) as full_name,
+    round(height/30.48,1) as height ,
+    round(weight*2.205,0) as weight,
+    birth_date,
     case when gender = 'M' then 'MALE'
     WHEN gender = 'F' THEN 'FEMALE'
     else 'unknown'
